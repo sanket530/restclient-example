2e28daf0251ce77ec8cabaea4893b48020a76e6e
Sanket Bhagwat <sanket@Sankets-MacBook-Air.local>
2024-12-15 15:04:12 +0530

    removed unnecessary commented code and removed use of deprecated classes

--- src/main/java/dev/sanket/myApplication/config/JokeRestClientConfig.java
+++ src/main/java/dev/sanket/myApplication/config/JokeRestClientConfig.java
@@ -6,8 +6,7 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.io.HttpClientConnectionManager;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
+import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
 import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -28,11 +27,8 @@ public class JokeRestClientConfig {
         SSLContext sslContext = SSLContextBuilder.create()
                 .loadTrustMaterial((chain, authType) -> true)
                 .build();
-        SSLConnectionSocketFactory sslSocketFactory = SSLConnectionSocketFactoryBuilder.create()
-                .setSslContext(sslContext)
-                .build();
         HttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
-                .setSSLSocketFactory(sslSocketFactory)
+                .setTlsSocketStrategy(new DefaultClientTlsStrategy(sslContext))
                 .build();
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setConnectionManager(connectionManager)
--- src/main/java/dev/sanket/myApplication/service/JokeService.java
+++ src/main/java/dev/sanket/myApplication/service/JokeService.java
@@ -14,92 +14,6 @@ public class JokeService {
     private final RestClient jokeRestClient;
     private final JokeRepository jokeRepository;
 
-//    public static void main(String[] args) {
-//        String str = "sanket";
-//        Set<Character> set = new HashSet<>();
-//        str.chars()
-//                .mapToObj(c -> (char) c)
-//                .filter(c -> !set.add(c))
-//                .forEach(System.out::println);
-//
-//        System.out.println();
-//        System.out.println(checkIsogram(str));
-//
-//        List<Employee> employees = List.of(
-//                new Employee(1, "Gangadhar", 10.0, "IT"),
-//                new Employee(2, "Akash", 20.0, "IT"),
-//                new Employee(3, "Akshay", 30.0, "Math"),
-//                new Employee(5, "Swati", 40.0, "Math"),
-//                new Employee(4, "Alok", 50.0, "Admin"),
-//                new Employee(6, "Sanket", 60.0, "Admin")
-//
-//        );
-//
-//        Map<String, Double> collect = employees.stream()
-//                .collect(Collectors.groupingBy(Employee::department,
-//                        Collectors.averagingDouble(Employee::salary)));
-//
-//        collect.forEach((dept, avgSalary) -> System.out.println("Department :" + dept + " average salary :" + avgSalary));
-//
-//        Map<String, Optional<Employee>> top = employees.stream()
-//                .collect(Collectors.groupingBy(Employee::department,
-//                        Collectors.reducing(BinaryOperator.maxBy(Comparator.comparingDouble(Employee::salary)))));
-//
-//        top.forEach((department, employee) -> System.out.println("Department :" + department + " emploee :" + employee));
-//
-//        Map<String, Integer> map1 = new HashMap<>(Map.of(
-//                "math", 40,
-//                "chem", 50,
-//                "physics", 45,
-//                "bio", 36,
-//                "english", 56
-//        ));
-//
-//        Map<String, Integer> map2 = Map.of(
-//                "math", 40,
-//                "physics", 52,
-//                "geography", 48,
-//                "history", 34,
-//                "english", 28
-//        );
-//
-//        Map<String, Integer> map3 = Stream.of(map1, map2)
-//                .flatMap(map -> map.entrySet().stream())
-//                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, Integer::sum));
-//
-//        map2.forEach((key, value) -> {
-//            map1.merge(key, value, Integer::sum);
-//        });
-//
-//        System.out.println(map1);
-//        System.out.println(map3);
-//
-//        int[] arr1 = {4, 7, 3, 5};
-//        int[] arr2 = {8, 2, 9, 1, 6};
-//
-//        int[] array = IntStream.concat(Arrays.stream(arr1), Arrays.stream(arr2))
-//                .sorted()
-//                .toArray();
-//        System.out.println(Arrays.toString(array));
-//
-//        int[] array1 = Stream.of(arr1, arr2)
-//                .flatMapToInt(Arrays::stream)
-//                .sorted()
-//                .toArray();
-//        System.out.println(Arrays.toString(array1));
-//
-//    }
-
-//    private static boolean checkIsogram(String str) {
-//        Set<Character> s = new HashSet<>();
-//        long count = str.toLowerCase()
-//                .chars()
-//                .mapToObj(c -> (char) c)
-//                .filter(c -> !s.add(c))
-//                .count();
-//        return count == 0;
-//    }
-
     public Joke getJoke() {
         Joke joke = jokeRestClient.get()
                 .uri("/random_joke")
@@ -116,10 +30,3 @@ public class JokeService {
         return jokeRepository.saveAll(jokes);
     }
 }
-
-//record Employee(Integer id, String name, Double salary, String department) implements Comparable<Employee> {
-//    @Override
-//    public int compareTo(Employee e) {
-//        return this.id().compareTo(e.id());
-//    }
-//}
