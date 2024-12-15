4e6eeb49567693840adeac0050ded0b4463a01e6
Sanket Bhagwat <sanket@Sankets-MacBook-Air.local>
2024-12-15 15:17:32 +0530

    removed unnecessary code

--- src/main/java/dev/sanket/myApplication/config/RestClientInterceptor.java
+++ src/main/java/dev/sanket/myApplication/config/RestClientInterceptor.java
@@ -1,18 +1,14 @@
 package dev.sanket.myApplication.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
@@ -47,43 +43,6 @@ public class RestClientInterceptor implements ClientHttpRequestInterceptor {
             log.debug("Headers      : {}", response.getHeaders());
             log.debug("Response body: {}", StreamUtils.copyToString(response.getBody(), StandardCharsets.UTF_8));
             log.debug("=======================response end=================================================");
-        }
-    }
-
-    private static class BufferingClientHttpResponseWrapper implements ClientHttpResponse {
-        private final ClientHttpResponse response;
-        private final byte[] body;
-
-        public BufferingClientHttpResponseWrapper(ClientHttpResponse response,
-                                                  byte[] body) {
-            this.response = response;
-            this.body = body;
-        }
-
-        @Override
-        public InputStream getBody() {
-            return new ByteArrayInputStream(body);
-        }
-
-        // Delegate other methods to wrapped response
-        @Override
-        public HttpStatusCode getStatusCode() throws IOException {
-            return response.getStatusCode();
-        }
-
-        @Override
-        public String getStatusText() {
-            return "";
-        }
-
-        @Override
-        public void close() {
-
-        }
-
-        @Override
-        public HttpHeaders getHeaders() {
-            return response.getHeaders();
         }
     }
 }
